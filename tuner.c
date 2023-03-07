aab8a8f111bad3fb321d415fa85c0c2423fa39f9
John Mears <john@themears.co.uk>
2023-03-07 18:05:40 +0000

    FIFO mode now works.

--- tuner.c
+++ tuner.c
@@ -9,6 +9,12 @@
 #define IMU_INT2_PIN 24
 #define IMU_INT1_PIN 23
 
+/*
+    FIFO data transfer:
+        The IMU chip behaves more like a QMI865A (rather than a C), documented here:
+            https://www.qstcorp.com/upload/pdf/202301/13-52-25%20QMI8658A%20Datasheet%20Rev%20A.pdf
+        The protocol for reading FIFO data is somewhat different, amd works.
+*/
 
 // Register addresses for FIFO according to the QMU8658 data sheet, different from the
 // sample code:
@@ -20,10 +26,19 @@ enum FIFO_REGISTERS {
     FIFO_DATA = 23
 };
 
-#define CTRL_CMD_REQ_FIFO 0x05
+typedef enum imu_commands {
+    CTRL_CMD_ACK = 0x00,
+    CTRL_CMD_RST_FIFO = 0x04,
+    CTRL_CMD_REQ_FIFO = 0x05,           // The data sheet confusingly also mentions 0x0D for this command. 5 seems to work.
+    NO_COMMAND = -1
+} imu_commands;
 
-#define FIFO_HIGH_WATER_THRESHOLD 64      // The FIFO High Water Threshold.
+#define FIFO_HIGH_WATER_THRESHOLD 16        // The FIFO High Water Threshold.
+#define FIFO_CTRL_VALUE 0x0D                // 0000 1101    stream mode, 128 samples FIFO size.
+#define FIFO_MAX_SAMPLES 128                // Must match previous definition.
 
+
+static void imu_command(imu_commands cmd);
 
 static void DrawCentredString_EN(UWORD Ystart, const char * pString, sFONT* Font, int fontWidth, UWORD Color_Foreground, UWORD Color_Background)
 {
@@ -31,43 +46,108 @@ static void DrawCentredString_EN(UWORD Ystart, const char * pString, sFONT* Font
     Paint_DrawString_EN((LCD_1IN28.WIDTH - len * fontWidth) >> 1, Ystart, pString, &Font24, Color_Foreground, Color_Background);
 }
 
-int counter = 0;
+static volatile int counter = 0;
 
-int read_pending = false;
+static volatile imu_commands pending_imu_command = NO_COMMAND;
 
 /**
- * This interrupt handle INT2 events, which generally indicate that data is ready
+ * This handles INT2 events, which generally indicate that data is ready
  * to be read from the IMU.
  */
-void imu_int2_handler(uint gpio, uint32_t events)
+static void imu_int2_handler()
 {
-
     unsigned char sample_count_ls, fifo_status;
     QMI8658_read_reg(FIFO_SMPL_CNT_LS, &sample_count_ls, 1);
     QMI8658_read_reg(FIFO_STATUS, &fifo_status, 1);
 
-    // Read the data: issue a host command that will result in INT1 when the data is ready to be read:
-    read_pending = true;
-    QMI8658_write_reg(QMI8658Register_Ctrl9, CTRL_CMD_REQ_FIFO);
+    volatile uint16_t sample_count = (uint16_t) sample_count_ls + (((uint16_t) (fifo_status & 0x03)) << 8);
+
+    // Read the data from the FIFO:
+    imu_command(CTRL_CMD_REQ_FIFO);
 
     counter++;
 }
 
 /**
- * This interrupt handle INT1 events, which generally indicates that a ctrl9 command has completed.
+ * This handles INT1 events, which indicates one of two things:
+ * - IMU device reset.
+ * - a ctrl9 command has completed.
  */
-void imu_int1_handler(uint gpio, uint32_t events)
+static void imu_int1_handler()
 {
     // Bit 0 should be set at this point; reading it clears it and the interrupt:
-    unsigned char stat1;
-    QMI8658_read_reg(QMI8658Register_Status1, &stat1, 1);
+    unsigned char status1;
+    QMI8658_read_reg(QMI8658Register_Status1, &status1, 1);
 
-    if (read_pending) {
-        read_pending = false;
-        // Finally we can actually read the data - HTW sets of values.
-        unsigned char data_buf[6];
-        QMI8658_read_reg(FIFO_DATA, data_buf, sizeof(data_buf));
+    volatile int i = status1;
+    
+    switch (pending_imu_command)
+    {
+        case NO_COMMAND:
+            // No command pending. Nothing to do.
+            i = 1;
+            break;
+
+        case CTRL_CMD_RST_FIFO:
+            // The FIFO has been reset.
+            i = 2;
+            break;
+
+        case CTRL_CMD_REQ_FIFO:
+            // Disable address auto increment:
+            QMI8658_write_reg(QMI8658Register_Ctrl1, 0x20); 
+
+            // See if the FIFO_rd_mode bit is 1 (it should be):
+            unsigned char FifoCtrl;
+            QMI8658_read_reg(FIFO_CTRL, &FifoCtrl, 1);
+
+            // Ack the command
+            QMI8658_write_reg(QMI8658Register_Ctrl9, CTRL_CMD_ACK);
+
+            // Read the data. In one test, I found 102 bytes ( = 6 * 17, and HWM was 16), byt looking at the values.
+            unsigned char data_buf[FIFO_MAX_SAMPLES * 6];
+            QMI8658_read_reg(FIFO_DATA, data_buf, sizeof(data_buf));    // Really we should read the number of samples.
+
+            // Clear the FIFO_rd_mode bit:
+            QMI8658_write_reg(FIFO_CTRL, FIFO_CTRL_VALUE);
+
+            break;
+
+        default:
+            i = 3;
+            break;        
     }
+
+    pending_imu_command = NO_COMMAND;
+}
+
+/** Interrupt event handler.
+ */
+void gpio_irq_dispatcher(uint gpio, uint32_t events)
+{
+    switch (gpio)
+    {
+        case IMU_INT1_PIN:
+            imu_int1_handler();
+            break;    
+        case IMU_INT2_PIN:
+            imu_int2_handler();
+            break;    
+    }
+}
+
+
+static void imu_command(imu_commands cmd)
+{
+    pending_imu_command = cmd;
+    QMI8658_write_reg(QMI8658Register_Ctrl9, cmd);
+}
+
+static void imu_command_spinwait()
+{
+    // Spin lock waiting for the command to complete via INT1:
+    while (pending_imu_command != NO_COMMAND)
+        ;
 }
 
 int main(void)
@@ -104,7 +184,7 @@ int main(void)
 
     LCD_1IN28_Display(ImageBuffer);
 
-    // TODO: extract the following code to a function.
+    // ***************** TODO: extract the following initialization code to a function.
 
     float acc[3], gyro[3];
     unsigned int tim_count = 0;
@@ -123,22 +203,33 @@ int main(void)
 
     // All inputs are disabled while we set things up.
 
+    // Set ourselves up to get the interrupts: callback, gpio config and IRQ bank:
+    gpio_set_irq_callback(&gpio_irq_dispatcher);
+    gpio_set_irq_enabled(IMU_INT1_PIN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled(IMU_INT2_PIN, GPIO_IRQ_EDGE_RISE, true);
+    irq_set_enabled(IO_IRQ_BANK0, true);
+
+    // Enable the accelerometer input:
+    QMI8658_enableSensors(QMI8658_CONFIG_ACC_ENABLE);
+
     // Enable FIFO with a HWM:
-    QMI8658_write_reg(FIFO_CTRL, 0x0D);     // 0000 1101    FIFO mode, 128 samples size.
-    QMI8658_write_reg(FIFO_WTM_TH, FIFO_HIGH_WATER_THRESHOLD);     // High water mark for generating interrupt INT2.
+    QMI8658_write_reg(FIFO_CTRL, 0x0D);                         // 0000 1101    FIFO mode, 128 samples size.
+    QMI8658_write_reg(FIFO_CTRL, FIFO_CTRL_VALUE);
+    QMI8658_write_reg(FIFO_WTM_TH, FIFO_HIGH_WATER_THRESHOLD);  // High water mark for generating interrupt INT2.
 
-    // Configure GPIO24 (INT2) as a source of interrupt: INT2 signals that data is ready to be read.
-    gpio_set_irq_enabled_with_callback(IMU_INT2_PIN, GPIO_IRQ_EDGE_RISE, true, &imu_int2_handler);
-    gpio_set_irq_enabled_with_callback(IMU_INT1_PIN, GPIO_IRQ_EDGE_RISE, true, &imu_int1_handler);
+    DEV_Delay_ms(3000);
 
-    // Finally enable just the accelerometer input:
-    QMI8658_config.inputSelection = QMI8658_CONFIG_ACC_ENABLE;
-    QMI8658_Config_apply(&QMI8658_config);
+    // Reset the FIFO:
+    // imu_command(CTRL_CMD_RST_FIFO);
+    // imu_command_spinwait();
 
+    DEV_Delay_ms(3000);
+
+    // ***************** 
 
     while (1)
     {
-        QMI8658_read_xyz(acc, gyro, &tim_count);
+    //    QMI8658_read_xyz(acc, gyro, &tim_count);
 
         // This seems to overwrite pre-existing numbers in the buffer:
         Paint_DrawNum(120, 90, acc[0], &Font16, 2, BLACK, WHITE);
@@ -149,48 +240,7 @@ int main(void)
         DEV_Delay_ms(100);
     }
 
-
-#if 0
-
-    float acc[3], gyro[3];
-    unsigned int tim_count = 0;
-    QMI8658_init();
-    printf("QMI8658_init\r\n");
-
-    while (true)
-    {
-        const float conversion_factor = 3.3f / (1 << 12) * 2;
-        uint16_t result = adc_read();
-        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
-        Paint_Clear(WHITE);
-        QMI8658_read_xyz(acc, gyro, &tim_count);
-        printf("acc_x   = %4.3fmg , acc_y  = %4.3fmg , acc_z  = %4.3fmg\r\n", acc[0], acc[1], acc[2]);
-        printf("gyro_x  = %4.3fdps, gyro_y = %4.3fdps, gyro_z = %4.3fdps\r\n", gyro[0], gyro[1], gyro[2]);
-
-        printf("tim_count = %d\r\n", tim_count);
-        Paint_DrawString_EN(30, 50, "ACC_X = ", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(30, 75, "ACC_Y = ", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(30, 100, "ACC_Z = ", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(30, 125, "GYR_X = ", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(30, 150, "GYR_Y = ", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(30, 175, "GYR_Z = ", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(30, 175, "GYR_Z = ", &Font16, WHITE, BLACK);
-        Paint_DrawNum(120, 50, acc[0], &Font16, 2, BLACK, WHITE);
-        Paint_DrawNum(120, 75, acc[1], &Font16, 2, BLACK, WHITE);
-        Paint_DrawNum(120, 100, acc[2], &Font16, 2, BLACK, WHITE);
-        Paint_DrawNum(120, 125, gyro[0], &Font16, 2, BLACK, WHITE);
-        Paint_DrawNum(120, 150, gyro[1], &Font16, 2, BLACK, WHITE);
-        Paint_DrawNum(120, 175, gyro[2], &Font16, 2, BLACK, WHITE);
-        Paint_DrawString_EN(50, 200, "BAT(V)=", &Font16, WHITE, BLACK);
-        Paint_DrawNum(130, 200, result * conversion_factor, &Font16, 2, BLACK, WHITE);
-
-        LCD_1IN28_Display(BlackImage);
-        DEV_Delay_ms(100);
-    }
-
-#endif
-
-    /* Module Exit */
+    // We never get here, but let's do the right thing:
     free(ImageBuffer);
     ImageBuffer = NULL;
 
